7241374e93672d72a28748e19574bb09fad3c51e
Kartheek Chowdary <Chowdarykarthik425@gmail.com>
2018-03-25 16:28:43 +0530

    Commit
    First Branch

--- GitProject/src/com/git/project/Sample1.java
+++ GitProject/src/com/git/project/Sample1.java
@@ -4,6 +4,7 @@ public class Sample1 {
 
 	public static void main(String[] args) {
 		System.out.println("Selenium Project");
+		System.out.println("we need to gear up for the Framework Design");
 
 	}
 
